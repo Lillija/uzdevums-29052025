fd46e79afb030648661f152a74f4dd2d043e92e1
Līga <asikeligalillija@gmail.com>
2025-05-30 10:03:12 +0300

    Methods in OOP PHP

--- Classes/Car.php
+++ Classes/Car.php
@@ -5,13 +5,20 @@ class Car{
 
     private $brand;
     private $color;
-    private $vehicleType = "Car";
+    public $vehicleType = "Car";
 
     //Constructor
-    public function __construct($brand,$color){
+    public function __construct($brand,$color = "none"){
         $this->brand = $brand;
         $this->color = $color;
     }
+
+    //Method
+    public function getCarInfo(){
+        return "Brand: " . $this->brand . ", Color: " . $this->color;
+    }
 }
 $car01 = new Car("Volvo", "green");
-$car01 = new Car("BMW");
+echo $car01->getCarInfo();
+echo "<br>";
+echo $car01->vehicleType;
